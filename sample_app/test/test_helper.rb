f17dce0dc1656ca22a60c6242e8197b5c6ed0dd0
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2015-10-04 23:55:56 +0900

    Sec.8, Remember me

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -14,4 +14,23 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !!session[:user_id]
   end
+
+  def log_in_as(user, options={})
+    password = options[:password] || "password"
+    remember_me = options[:remember_me] || "1"
+    if integration_test?
+      post login_path, session: {
+        email: user.email,
+        password: password,
+        remember_me: remember_me
+      }
+    end
+    session[:user_id] = user.id
+  end
+
+  private
+
+    def integration_test?
+      defined? post_via_redirect
+    end
 end
